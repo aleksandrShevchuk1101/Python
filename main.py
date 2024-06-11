0c12c52249bb41f239e6103152d91868cdb5e8a3
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-11 15:13:02 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_letter_grade(score):
+    letter_grade = 'A' if score >=90 else 'B' if score >= 80 'C' else 'D' if score >=70 else 'E'
+    return letter_grade
+
+
+
+print(get_letter_grade(90))
+print(get_letter_grade(80))
+print(get_letter_grade(70))
+print(get_letter_grade(60))
+
+
