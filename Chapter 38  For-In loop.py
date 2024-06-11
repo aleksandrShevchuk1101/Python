7cf9ca34866436c93fa153cc34777c3ba4019596
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-11 18:33:59 -0400

    afsasdf

---
+++ Chapter 38  For-In loop.py
@@ -0,0 +1,71 @@
+""" my_list = [True, 10, 'abc', {}]
+
+for foo in my_list:
+    print(foo)
+
+
+my_object = {'x': 10, 'y': True, 'z':'abc'}
+
+
+for foo in my_object:
+    print(foo, my_object[foo])
+
+
+my_name = 'Aleksandr'
+
+for goo in my_name:
+    print(goo)
+
+for num in range(5):
+    print(num)
+
+for odd_num in range(3,10,2):
+    print(odd_num)
+
+
+print('---------------------------------------------------------------')
+
+
+countries = ['USA', 'Canada', 'UK', 'Australia']
+
+for county in countries:
+    print(f"Welcome to {county}")
+
+print('---------------------------------------------------------------')
+ 
+months= ('Jan', 'Feb', 'Apr', 'Sep', 'Foo')
+
+for moth in months:
+    print(f"Current month is {moth}")
+
+
+print(moth) """
+
+
+""" products_prices = {
+    'PC': 1000,
+    'Mobile Phone': 20,
+    'Tablet': 350,
+    'Camera': 700
+} """
+
+""" for key in products_prices:
+    print(key, products_prices[key]) """
+
+""" for k, v in products_prices.items():
+    print(k, v) """
+
+
+
+
+""" name = "adf;adslfjasd;lfj;l"
+
+for char in name:
+    print(char) """
+
+for i in range(10, 0, -1):
+    print(i)
+   
+
+
+     
