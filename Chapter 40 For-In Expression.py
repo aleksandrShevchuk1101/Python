336bbe1ce91fe1926607a41d4d5d6ff5a3bc4882
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-14 19:25:58 -0400

    Chapter 40

---
+++ Chapter 40 For-In Expression.py
@@ -0,0 +1,26 @@
+""" all_nums = [-3,1,0,10,-20,5]
+
+absolute_nums = [abs(item) for item in all_nums]
+
+print(absolute_nums)
+
+print(all_nums) """
+
+
+""" all_nums = [-3, 1, 0 , 10 , -20, 5]
+
+positive_nums = []
+
+for num in all_nums:
+    if num > 0:
+        positive_nums.append(num)
+
+
+print(positive_nums)
+
+print(all_nums) """
+
+
+
+
+
