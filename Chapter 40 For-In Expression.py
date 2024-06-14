b2edfab9888ff0cb41dcf5af21620ffd5002180b
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-14 19:55:32 -0400

    New commit

--- Chapter 40 For-In Expression.py
+++ Chapter 40 For-In Expression.py
@@ -21,6 +21,98 @@ print(positive_nums)
 print(all_nums) """
 
 
+""" all_nums = [-3, 1,0,10,-20,5]
+
+positive_nums=[ num for num in all_nums if num > 0 ]
+
+print(positive_nums)
+print(all_nums)  """
+
+
+""" my_set = {1,10, 15}
+
+new_set = set()
+
+
+for val in my_set:
+    new_set.add(val*val)
+
+print(my_set)
+
+print(new_set) """
+
+
+""" my_set = {1,10,15}
+
+new_set = {item* item for item in my_set}
+
+print(new_set)
+print(my_set) """
+
+
+
+""" my_scores = {
+    'a': 10,
+    'b':7,
+    'm':14
+}
+
+
+scores = {}
+
+for k, v in my_scores.items():
+    scores[k] = v
+
+
+print(scores)
+
+print(my_scores) """
+
+
+
+
+""" my_scores = {
+    'a': 10,
+    'b': 7,
+    'm': 14
+}
+
+scores = {k: v * 10 for k,v in my_scores.items()}
+
+
+print(my_scores)
+
+print(scores) """
+
+""" nums = [10, 2, 5, 100]
+
+
+squared_nums = [pow(num,2) for num in nums]
+
+"" for item in nums:
+    squared_nums.append(pow(item,2)) """
+
+#print(squared_nums) 
+
+
+
+
+
+
+
+
+
+
+ 
+
+  
+
+
+
+
+ 
+
+ 
 
 
 
