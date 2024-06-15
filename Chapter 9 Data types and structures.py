32d02b5eea28cbfec9075474df2bdf81e949c47e
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-15 19:01:44 -0400

    ssadfa

---
+++ Chapter 9 Data types and structures.py
@@ -0,0 +1,38 @@
+
+
+# my_name = "Aleksandr"
+# foo = 35
+
+
+# print(id(my_name))
+# print(id(foo)) 
+
+
+# my_name = "Aleksandr"
+# print(id(my_name))
+
+# my_num = 777
+# print(id(my_num))
+
+# other_num = my_num
+# print(id(other_num)== id(my_num))
+
+
+# str
+# int
+# bool
+# list
+# dict
+
+
+# my_num = 777
+# print(type(my_num) == type(int))
+
+# my_photos = ["afadfa",'asfdasdf']
+# print(type(my_photos))
+
+# print(type(int))
+
+print(isinstance(777, int))
+print(isinstance("adfadsf", str))
+print(isinstance(777, object))
