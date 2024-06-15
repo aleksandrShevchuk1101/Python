add11075c3b3bcaa5758a73258035b9fd1bb75b3
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-15 19:01:33 -0400

    sfsdafas

---
+++ Chapter 10 Strings.py
@@ -0,0 +1,49 @@
+
+
+# info_msg= """adsflasdfj;asldjf 
+# asdfals;dfjasd;lfj;lfj
+# ja;sfjkd;adsfj;lfj
+# ajksdfl;jasdf;ljkasd;"""
+
+
+# print(info_msg)
+
+
+# my_name = "aleksandr"
+# print(len(my_name))
+
+# print(my_name[0])
+# print(my_name.capitalize())
+
+
+# long_str = "This is a \
+#         very long string"
+
+# print(long_str)
+# print(type(long_str) == str)
+# print(id(long_str))
+
+
+my_comment = "This is my short comment"
+
+print(len(my_comment))
+
+
+
+print(my_comment)
+print(my_comment.replace('short',"long"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
