52fabae9c96144918423d9e34b1711ef448347ba
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-16 19:10:36 -0400

    dfas

---
+++ Chapter 12 Numeric types.py
@@ -0,0 +1,44 @@
+
+# friends_qty = 50
+
+# print(friends_qty)
+# print(type(friends_qty))
+
+
+# base = 5
+# power = 3
+
+# print(pow(base, power))
+# print(base**power)
+
+
+# one_million = 1_000_000
+# print(one_million)
+
+# my_number = 3_427
+# print(my_number)
+
+
+# average_rating = 4.9
+
+# print(type(average_rating))
+
+# print(average_rating.is_integer())
+
+# print(round(average_rating))
+
+# print(int(average_rating))
+
+# print(float(1000))
+
+
+
+
+complex_a = 3 + 5j
+complex_b = 4 + 7j
+
+sum = complex_a + complex_b
+
+print(sum)
+
+print(type(sum ))
