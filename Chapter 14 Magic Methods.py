59dd8c197c9e27ff8f86b1be12e93349cd57c073
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-16 20:34:21 -0400

    end the chapter

---
+++ Chapter 14 Magic Methods.py
@@ -0,0 +1,13 @@
+
+# my_str = "abc"
+
+# print(dir(str))
+
+#print(list.__doc__)
+
+my_num = 10
+other_num = 10
+
+print(my_num.__eq__(other_num))
+print(my_num == other_num)
+print(help(my_num.__add__))
