cf59aad8e047571b1958ca5c9db83416ea507804
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-16 20:16:08 -0400

    end chapted

---
+++ Chapter 13 Boolean Values.py
@@ -0,0 +1,43 @@
+
+
+# db_is_available = False
+
+# print(db_is_available)
+# print(type(db_is_available))
+
+
+# print(bool(10))  # False
+# print(bool(0)) # False
+# print(bool(0.0))  # False
+# print(bool("abc"))
+# print(bool(""))
+
+# print(100 > 50)
+# print(100 == 50)
+# print([1, 2, 3] == [1, 2, 3, 4])
+# print([1, 2, 3] == [1, 2, 3])
+
+# is_available = True
+# print(not is_available)
+
+
+
+# my_name = "Aleksandr"
+
+# if bool(is_available and my_name):
+#     print("Is available")
+
+
+# print(is_available and my_name)
+
+int_num = 5 
+float_num = 4.5
+
+print(int_num + float_num)  # 9.5
+
+
+print(int_num.__add__(float_num))
+print(float_num.__radd__(int_num))
+
+
+
