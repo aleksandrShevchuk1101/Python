0e2e18b0cf410a43830d43634837992af16621d6
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-16 21:24:19 -0400

    first part with lists. Finished in 003

---
+++ Chapter 15 Lists.py
@@ -0,0 +1,64 @@
+
+# my_fruits = ["apple", "banana", "lime"]
+# posts_ids = [151, 245, 762, 167]
+# user_inputs = [True, "hi!", 10.5]
+
+
+# print(my_fruits)
+
+# del my_fruits[0]
+# print(my_fruits)
+# print(len(my_fruits))
+
+
+# users = [
+#     {
+#         "user_id": 134,
+#         "user_name": "Alice"
+#     },
+#     {
+#         "user_id": 831,
+#         "user_name": "Bob"
+#     }
+# ]
+
+# print(len(users))
+# print(users[1]["user_name"])
+
+
+# inp = [True, "Hi!", 'asdf', 10.5]
+
+# inp.pop()
+# print(inp)
+
+# inp.pop(0)
+# print(inp)
+
+
+# posts_ids = [245, 151, 762, 167]
+
+# posts_ids.sort()
+
+# print(posts_ids)
+
+# posts_ids.sort(reverse=True)
+
+# print(posts_ids)
+
+# greeting = "Hello from Python"
+
+# foo = list(greeting)
+
+# print(foo)
+
+
+ratings = [2.5, 5.0, 4.3, 3.7]
+
+print(min(ratings))
+print(max(ratings))
+print(sum(ratings))
+
+print(sum(ratings) / len(ratings))
+
+
+
