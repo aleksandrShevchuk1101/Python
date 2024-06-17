75a7742ad581d7eb0ff843415a37f01554311881
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-17 17:37:21 -0400

    Tuples, strange stuff

---
+++ Chapter 17 Tuples.py
@@ -0,0 +1,35 @@
+
+# my_fruits = ("apple", "apple", "foo")
+
+# print(my_fruits)
+# print(id(my_fruits))
+
+# my_fruits[4] = "safd "
+
+
+# posts_id = (151, 151, 762, 245)
+
+# print(posts_id.count(151))
+# print(posts_id.index(245))
+
+
+# posts_ids = (151, 245)
+
+# posts_ids_list = list(posts_ids)
+# posts_ids_list.append(777)
+
+# print(posts_ids_list)
+
+# post_ids_tuple = tuple(posts_ids_list)
+# print(post_ids_tuple)
+
+
+
+
+screen_res = (1920, 1080)
+# print(screen_res)
+# print(type(screen_res))
+# print(isinstance(screen_res, tuple))
+
+print(screen_res.index(1080))
+print(screen_res.count(1080))
