8a023f4680ea72bf8c648b1c4ef386dc20c9b69b
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-17 21:20:45 -0400

    Ended with ranges

---
+++ Chapter 19 Ranges.py
@@ -0,0 +1,39 @@
+# my_range = range(7)
+
+# print(type(my_range))
+
+# print(my_range)
+
+# print(list(my_range)) 
+
+# my_range = range(10,20,2)
+
+# print(my_range)
+
+# print(list(my_range))
+
+# my_range = range(10, 20, 3)
+
+# for n in my_range:
+#     print(n)
+
+
+# my_range = range(1,1000, 2)
+
+# print(my_range)
+
+
+# for n in my_range:
+#     print(n)
+
+
+# my_range = range(1,1000)
+# even_nums = range(0, 101, 2)
+# print(list(my_range))
+# print(list(even_nums))
+
+odd_nums = range(1, 100, 2)
+
+print(odd_nums.index(3))
+
+print(odd_nums[0])
