48eaa0346e110f074e5d064c3129b581b3258e83
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-20 18:03:46 -0400

    Ended chapter

---
+++ Chapter 20 Working with Sequances.py
@@ -0,0 +1,47 @@
+# fruits = ['apple', 'banana', 'lime']
+
+# quantities = [100, 70, 50]
+
+# fruit_qtys_zip = zip(fruits, quantities)
+
+# # print(fruit_qtys_zip)
+# # print(type(fruit_qtys_zip))
+
+# # fruit_qtys_zip = list(fruit_qtys_zip)
+
+# # print(fruit_qtys_zip)
+
+
+# fruit_qtys_dict = dict(fruit_qtys_zip)
+# print(fruit_qtys_dict)
+
+
+# products = ['phone', 'tablet', 'laptop']
+
+# quantities = [343, 13, 74, 12]
+
+# foo = zip(products, quantities)
+
+# print(foo)
+# print(type(foo))
+
+# products_qtys_list = list(foo)
+# print(products_qtys_list)
+# print(type(products_qtys_list))
+
+
+# for product in products_qtys_list:
+#     print(product[0])
+
+# products = ['phone', 'tablet', 'laptop']
+
+# quantities = (343, 13)
+
+# products_qtys = zip(products,quantities)
+
+# products_qtys_dict = dict(products_qtys)
+
+# print(products_qtys_dict)
+
+
+ 
