d6bb8b3ab5c4cb4cbd91cf808d265bafe223d9a6
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-20 21:05:56 -0400

    Ended chapter 23

---
+++ Chapter 23 Function Arguments.py
@@ -0,0 +1,35 @@
+
+
+
+# def print_fruits_info(person_name, fruits):
+#     fruits_copy = fruits.copy()
+#     fruits_copy.pop()
+#     for item in fruits_copy:  
+#         print(person_name, item)
+
+
+# my_name='Bogdan'
+# favorite_fruits=["oranges", 'apples', 'bananas']
+
+# print_fruits_info(my_name, favorite_fruits)
+# print(favorite_fruits)
+
+
+# def my_fn(first, second=True):
+#     print(first)
+#     print(second)
+
+# my_fn(None)
+
+
+
+# def merge_lists_to_dist(first:list, second:list): return dict(zip(first, second))
+
+
+
+# res = merge_lists_to_dist([1,2,4], [1,2,3])
+
+# print(res)
+
+
+   
