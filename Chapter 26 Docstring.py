7e341fb9de788783a7e946aa81c01ce6275adc4c
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-22 19:02:33 -0400

    Ended with docstring

---
+++ Chapter 26 Docstring.py
@@ -0,0 +1,25 @@
+# def mult_by_factor(value, mult=1):
+#     """Multiplies number by multiplicator, lol"""
+#     return value * mult
+
+
+# mult_by_factor()
+
+
+
+
+# def print_number_info(num):
+#     """
+#         The discription
+
+#         Args:
+#             num(int): Number of something
+#     """
+
+#     return "lol"
+
+
+# print_number_info(50)
+
+
+import math
