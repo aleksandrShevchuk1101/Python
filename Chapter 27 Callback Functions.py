bb1e5a13236e794e89c42496ba53afc2ed5781e2
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-22 20:14:23 -0400

    Finished with callback functions

---
+++ Chapter 27 Callback Functions.py
@@ -0,0 +1,25 @@
+# def print_number_info(num):
+#     if(num % 2) == 0:
+#         print("The value is even")
+#     else:
+#         print("The value is odd")
+
+# def process_number(num, callback_fn):
+#     callback_fn(num)
+
+
+# num = 3
+# process_number(num, print_number_info)
+
+
+# def send_data(data):
+#     pass
+
+# def process_data(input_data, send_data_fn):
+#     uptated_data = input_data.copy()
+#     send_data_fn(uptated_data)
+
+
+# process_data({'name': "Alex"}, send_data)
+
+
