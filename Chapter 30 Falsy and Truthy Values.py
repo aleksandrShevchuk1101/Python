337f2c64a894687013127ddb1ac7ad12c9cb051e
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-23 13:19:34 -0400

    Finished with falsy and Truthy values

---
+++ Chapter 30 Falsy and Truthy Values.py
@@ -0,0 +1,13 @@
+print(bool(1))
+print(bool(1.0))
+print(bool(2 j))
+
+print(bool(True))
+# print(bool(None))
+
+print(bool("dfsd"))
+print(bool([1,2]))
+print(bool({"sdf":1}))
+print(bool((1,2)))
+print(bool({12,3}))
+print(bool(range(4)))
