60a6408ee88d4438191204f34743aef8267bb26b
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-25 16:26:47 -0400

    Finished with the chapter

---
+++ Chapter 31 Logical and Comparison Operators.py
@@ -0,0 +1,17 @@
+# my_list = [1,2]
+
+# print(bool(my_list))
+# print(not bool(my_list))
+# print(not not bool(my_list))
+
+# print('asdf' and print("dsaf") and  'asdf' and 'asdfads')
+
+ 
+
+my_list = []
+
+other_list = [1]
+
+print(my_list or other_list)
+print(not not my_list or other_list)
+
