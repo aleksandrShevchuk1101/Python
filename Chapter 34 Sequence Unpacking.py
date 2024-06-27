a41408029841bdce450d59de87ed8c66588bdf94
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-27 18:48:06 -0400

    Finished the chapter

---
+++ Chapter 34 Sequence Unpacking.py
@@ -0,0 +1,65 @@
+# employee_info = ("Jane Doe", 32, "Junior Developer")
+
+# name= employee_info[0]
+# age = employee_info[1]
+# position = employee_info[2]
+
+# name, age, position = employee_info  # Unpacking
+
+# print(name, age, position)
+
+# color = (225, 128, 10)
+
+# red, green, blue = color
+
+# print(red)
+# print(green)
+# print(blue)
+
+# color = (red, green, blue)
+# print(color)
+
+# user_credentials = [
+#     ('user1', 'password1'),
+#     ('user2', 'password2'),
+#     ('user3', 'password3')
+# ]
+
+
+# user1, user2, user3 = user_credentials
+
+# print(user1)
+# print(user2)
+# print(user3)
+
+# user1_name, user1_pas = user1
+# user2_name, user2_pas = user2
+# user3_name, user3_pas = user3
+
+
+# person = ('Bod', 25)
+
+# name, age, *other = person 
+
+
+# school_grades = (80, 75, 35, 20, 90)
+
+# first, *middle, last = school_grades
+
+# print(first)
+# print(middle)
+# print(last)
+
+# def calc(lenght: int, width: int):
+#     return lenght * width
+
+# dimensions = [100, 20]
+
+# area = calc(dimensions[0], dimensions[1])
+# print(area)
+
+
+# area = calc(*dimensions)
+# print(area)
+
+
