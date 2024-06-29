c29e1a763b99b693f018cae5e4fdd154378a4412
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-29 16:05:15 -0400

    Finished with for in loop

--- Chapter 38  For-In loop.py
+++ Chapter 38  For-In loop.py
@@ -1,74 +1,119 @@
-""" my_list = [True, 10, 'abc', {}]
+# coutries = ["USA", "Canada", "UK", "Australia"]
 
-for foo in my_list:
-    print(foo)
-
-
-my_object = {'x': 10, 'y': True, 'z':'abc'}
-
-
-for foo in my_object:
-    print(foo, my_object[foo])
-
-
-my_name = 'Aleksandr'
-
-for goo in my_name:
-    print(goo)
-
-for num in range(5):
-    print(num)
-
-for odd_num in range(3,10,2):
-    print(odd_num)
-
-
-print('---------------------------------------------------------------')
-
-
-countries = ['USA', 'Canada', 'UK', 'Australia']
-
-for county in countries:
-    print(f"Welcome to {county}")
-
-print('---------------------------------------------------------------')
- 
-months= ('Jan', 'Feb', 'Apr', 'Sep', 'Foo')
-
-for moth in months:
-    print(f"Current month is {moth}")
-
-
-print(moth) """
-
-
-""" products_prices = {
-    'PC': 1000,
-    'Mobile Phone': 20,
-    'Tablet': 350,
-    'Camera': 700
-} """
-
-""" for key in products_prices:
-    print(key, products_prices[key]) """
-
-""" for k, v in products_prices.items():
-    print(k, v) """
+# for a in coutries:
+#     print(f"Welcome to {a}")
 
 
 
+# months = ("Jan", "Feb", "Apr", "September")
 
-""" name = "adf;adslfjasd;lfj;l"
-
-for char in name:
-    print(char) """
-
-""" for i in range(10, 0, -1  ):
-    print(i) """
+# for item in months:
+#     print(item)
 
 
- 
-   
+# foo = {
+#     'PC': 1000,
+#     'Mobile Phone': 200,
+#     'Tablet': 350,
+#     'Camera': 700
+# }
+
+# for k in foo:
+#     print(k)
+#     print(foo[k])
+
+# for item in foo.items():
+#     print(item)
+
+# for k,v in foo.items():
+#     print(k,v)
+
+# name = 'Jennifer'
+
+# for char in name:
+#     print(char)
 
 
-     
+# for i in range(10,-1,-1):
+#     print(i)
+
+# print("Racket launch")
+
+# foo = {'romance', 'fantasy', 'comedy', 'Science Fiction'}
+
+
+# for item in foo:
+#     print(item)
+
+
+
+# def dict_2_list(foo: dict):
+
+#     if not type(foo) is dict:
+#         raise TypeError("Wrong type")
+    
+#     foo1 = []
+
+#     for k,v in foo.items():
+#         temp = (k,  v *2 if type(v) is int else v)
+#         foo1.append(temp)
+        
+
+
+#     return foo1
+
+
+# foo = {
+#     "one" : 1,
+#     "two" : "sdfasdf",
+#     "three" : 3,
+#     "four" : 'sdafasdf'
+# }
+
+# foo1 = {
+#     "one" : 234,
+#     "two" : 234,
+#     "three" : False,
+#     "four" : 33.2
+# }
+
+
+# try:
+#     print(dict_2_list(foo))
+#     print(dict_2_list(foo1))
+# except Exception as e:
+#     print(e)
+# else:
+#     print("Success")
+# finally:
+#     print("Finaly block")
+
+
+# def filter_list(foo1: list, foo2):
+
+#     type1 = type(foo1)
+#     type2 = type(foo2)
+
+#     if type1 is not list:
+#         print("Error")
+#         return []
+
+#     foo = []
+
+#     for item in foo1:
+       
+#        if type(item) == type(foo2):
+#             foo.append(item)
+
+
+#     return foo
+
+
+
+# data = [354,"hjfhgj", 23.5, False, 22.6, 'asdf', 12234, 53535, (2,3,4,5,6)]
+# print(filter_list(data, 1243))
+# print(filter_list(data, bool))
+# print(filter_list(data, float))
+# print(filter_list(data, str))
+# print(filter_list(data, tuple))
+
