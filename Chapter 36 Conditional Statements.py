88f51e8aaee0deb7ac2ed9ed722a98fd85b3c533
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-06-29 12:47:21 -0400

    vxcvc

---
+++ Chapter 36 Conditional Statements.py
@@ -0,0 +1,31 @@
+# person_info = {
+#     'age': 20
+# }
+
+# if not person_info.get("age"):
+#     print("There's no key")
+
+
+# weight = 15
+
+# if weight <= 0:
+#     print("Invalid weight. Weight must be greater than zero")
+
+
+# if 0 < weight <= 5:
+#     print("The shipping fee is ")
+
+
+# if 5 < weight <= 15:
+#     print("The shipping fee is 10$")
+
+# if 15 < weight <= 30:
+#     print("sadfasdf")
+
+
+# if weight > 30:
+#     print("More than 30")
+
+
+
+
