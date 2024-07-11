3e5655db753d8dfec94ef4bd86eb5f40452a11a9
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-07-11 19:40:23 -0400

    Finished with the chapter

---
+++ Chapter 56  Working with Dates and Times.py
@@ -0,0 +1,72 @@
+# import datetime
+
+# future_date = datetime.date(2022, 10, 12)
+# print(future_date)
+
+# print(future_date.day)
+
+# some_time = datetime.time(15, 30, 10)
+# print(some_time)
+
+
+# print(some_time.hour)
+# print(some_time.minute)
+# print(some_time.second)
+
+# from datetime import datetime
+
+# future_datetime = datetime(2100, 10, 12, 20, 23, 50)
+# print(future_datetime)
+
+# # print(dir(future_datetime))
+
+# print("Formated date, ", future_datetime.strftime("%B %d, %Y") )
+# print("Formated time:", future_datetime.strftime("%I"))
+# print("Formated date and time:",  )
+
+
+# from datetime import datetime
+
+# foo = "3000-11-20 14:25:45"
+
+# parsed_datetime = datetime.strptime(foo, "%Y-%m-%d %H:%M:%S")
+# print(parsed_datetime)
+# print(parsed_datetime.year)
+
+
+# parsed_data = datetime.strptime("June 03, 2050", "%B %d, %Y")
+# print(parsed_data)
+# print(datetime.now())
+
+# from datetime import datetime, timedelta
+
+# future_datetime = datetime(2100, 1, 1, 18, 10, 45)
+# print(future_datetime)
+
+# print(future_datetime + timedelta(days=100))
+# print(future_datetime + timedelta(hours=3))
+# print(future_datetime + timedelta(days=3, hours = 5, minutes= 50))
+
+
+# print(future_datetime - timedelta(days=5))
+
+import time 
+
+
+# currnet_time_in_seconds = print(time.time())
+# print(time.ctime(currnet_time_in_seconds))
+
+
+# print(time.ctime(2353452345))
+
+start_time = time.time()
+# print('Start')
+
+# time.sleep(3)
+
+my_range = list(range(100_000_000))
+
+end_time = time.time()
+
+print("Continue", round(end_time - start_time, 1), "sec")
+   
