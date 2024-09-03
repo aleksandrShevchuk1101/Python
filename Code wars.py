026924b34933116c1e631832ffa3ed36bcb2ed19
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2024-09-02 21:02:54 -0400

    fasdf

--- Code wars.py
+++ Code wars.py
@@ -59,13 +59,106 @@
 
 
 
-def foo(s):
-    if not isinstance(s, str):
-        raise TypeError
+# def foo(s):
+#     if not isinstance(s, str):
+#         raise TypeError
     
-    return s.upper()
+#     return s.upper()
     
 
 
 
-print(foo('dsf'))
+# print(foo('dsf'))
+
+# import re
+
+# res = re.split(r'\+|=', foo)
+# res = [int(item[::-1]) for item in res]
+
+# # print(int(res[0]+int(res[1]) == int(res[2])))
+
+
+
+# print(res[0]+res[1] == res[2])
+
+# foo1 = [''.join(item.) for item in res]
+
+
+# foo = '73+42=16'
+# foo = '5+8=13'
+
+# res = foo.replace('+',',').replace('=',',').split(',')
+# res = [int(i[::-1]) for i in res]
+
+# print(res[0] + res[1]== res[2])
+
+# res = foo.split('[+=]')
+# print(res)
+
+
+#--------------------------------------------------------------
+# class Dislike:
+#     pass
+
+# class Like:
+#     pass
+
+# class Nothing:
+#     pass
+
+
+# arr = [Dislike]
+# # arr = [Like, Like]
+# # arr = [Dislike, Like]
+
+
+# def like_or_dislike(lst):
+
+#     dis = False
+#     like = False
+
+#     for item in lst:
+#         if type(item) is Dislike:
+#             like = False
+#             dis = not dis
+
+#         if type(item) is Like:
+#             dis = False
+#             like = not like
+
+#     return Dislike if dis else Like if like else Nothing
+
+# print(like_or_dislike(arr))
+
+#--------------------------------------------------------------
+
+# foo = ['B', 'C', "D", F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z.]
+
+# lol = 'x' in foo
+
+# print(lol)
+
+# def disemvowel(string_):
+
+#     list_of_vowels = ('a', 'e', 'i', 'o', 'u')
+#     # new_string = ""
+
+#     # for i in string_:
+#     #     if i.lower() not in list_of_vowels:
+#     #         new_string += i
+
+
+
+
+#     return ''.join([i for i in string_ if i.lower() not in list_of_vowels])
+
+# print(disemvowel("This website is for losers LOL!"))
+# disemvowel("No offense but,\nYour writing is among the worst I've ever read")
+
+
+
+# def lol(s):
+#    for i in "aeiouAEIOU":
+#         s = s.replace(i,'')
+
+#     return s
