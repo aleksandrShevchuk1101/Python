dc94944b718621138610aff223b55ccb0b6694ec
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2025-03-22 17:24:22 -0400

    Finished several chapters

---
+++ main.py
@@ -0,0 +1,28 @@
+# print('hello Python')
+# print("Hello Python")
+
+
+# print(100)
+# print(-20)
+
+# print(4.4)
+
+# print(True)
+# print(False)
+
+
+# print([])
+# print([1,2,3,4])
+
+# print({})
+# print({"name": 'Alex'})
+# print({
+#     'name': 'Alex',
+#     'is_instructor': {},
+#     'afa' : []
+#     })
+
+name = "alex"
+
+print(dir(name))
+print(name.capitalize())
