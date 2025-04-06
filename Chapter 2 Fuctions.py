e60001fb2ab19843a3aad3fb09cff4d41d029b74
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2025-04-05 21:42:07 -0400

    Added the new chapter 4

---
+++ Chapter 2 Fuctions.py
@@ -0,0 +1,41 @@
+# def hello(name: str):
+#     print(name.upper())
+
+
+
+# hello("Who are you?")
+
+
+# def add_numbers(a, b):
+#     sum = a + b
+#     print(sum)
+
+
+# add_numbers(5, 2)
+# add_numbers(100.6, 50)
+
+# def multiply_numbers(a, b):
+#     result = a * b
+#     return result
+
+
+# result_of_multiplication = multiply_numbers(20, 5)
+
+# print(result_of_multiplication)
+
+# print(multiply_numbers(15,2))
+
+
+
+
+# print(print(input))
+
+# print(dir(__builtins__))
+
+
+# input("Please enter your name")
+
+print(type(True))
+
+
+
