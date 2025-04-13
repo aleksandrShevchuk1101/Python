f6573bc05bc80be05e926968d51d92b403d8794c
aleksandr Shevchuk <aleksandr.shevchuk1101@gmail.com>
2025-04-13 19:55:29 -0400

    Str class

--- Chapter 10 Strings.py
+++ Chapter 10 Strings.py
@@ -1,49 +1,26 @@
 
-
-# info_msg= """adsflasdfj;asldjf 
-# asdfals;dfjasd;lfj;lfj
-# ja;sfjkd;adsfj;lfj
-# ajksdfl;jasdf;ljkasd;"""
-
-
-# print(info_msg)
-
-
-# my_name = "aleksandr"
-# print(len(my_name))
-
-# print(my_name[0])
-# print(my_name.capitalize())
-
-
-# long_str = "This is a \
-#         very long string"
+# long_str = '''This is' a very
+#                 long string'''
 
 # print(long_str)
-# print(type(long_str) == str)
+# print(type(long_str))
+# print(type(long_str)==str)
 # print(id(long_str))
 
-
-my_comment = "This is my short comment"
-
-print(len(my_comment))
+# print(long_str.capitalize())
 
 
+baz = "This is my short comment"
+# print(baz)
 
-print(my_comment)
-print(my_comment.replace('short',"long"))
+# print(len(baz))
 
+# xyz = baz.replace('short', 'long')
+# print(xyz)
 
+print(baz.count('is'))
+print(baz[:-1])
+print(baz.upper())
 
+print(dir(baz))
 
-
-
-
-
-
-
-
-
-
-
- 
